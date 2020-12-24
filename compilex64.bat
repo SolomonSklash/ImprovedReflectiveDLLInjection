8a9dfb54d6030c981a89178a4f589936c0d410a5
Solomon Sklash <solomonsklash@0xfeed.io>
2020-12-23 21:46:51 -0800

    Updated compile script.

--- compilex64.bat
+++ compilex64.bat
@@ -1,3 +1,5 @@
-cl.exe /nologo /GL /O1 /MT /W4 /GS- /permissive- /diagnostics:caret /D_USRDLL /D_WINDLL /Tc dll/src/*.c /link "User32.lib" /MACHINE:x64 /DLL /OUT:reflectivex64.dll
+cl.exe /nologo /GL /O1 /MT /W4 /GS- /permissive- /diagnostics:caret /D_USRDLL /D_WINDLL /Tc dll/src/*.c /link "User32.lib" /MACHINE:x64 /DLL /OUT:reflective-x64.dll
 
 del *.obj
+del *.lib
+del *.exp
