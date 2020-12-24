a01083cdc49281ca6607c5e3b9283e9e1130b4be
Solomon Sklash <solomonsklash@0xfeed.io>
2020-12-23 21:59:47 -0800

    Added MingW compile script for x64 and x86.

---
+++ compile-x64.sh
@@ -0,0 +1,1 @@
+x86_64-w64-mingw32-gcc -shared -m64 -Os --static -Wall -Wno-unknown-pragmas dll/src/*.c -o reflective-mingw-x64.dll
---
+++ compile-x86.sh
@@ -0,0 +1,1 @@
+i686-w64-mingw32-gcc -shared -Os --static -Wall -Wno-unknown-pragmas dll/src/*.c -o reflective-mingw-x64.dll
